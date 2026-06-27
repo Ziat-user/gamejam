40a9a94a9dff478291fe05cad325805cc4b35794
Yuki-Ikeda-0703 <partenon4126@gmail.com>
2026-06-27 12:09:01 +0900

    フォルダを追加しました

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    //public Player player;
+    //public ToyCar toyCar;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        //if(player.)
+    }
+}

1b217e723568cddcfd8524ec841b8004ad747757
Yuki-Ikeda-0703 <partenon4126@gmail.com>
2026-06-27 14:39:05 +0900

    助けて

--- Assets/C#/Toycar.cs
+++ Assets/C#/Toycar.cs
@@ -8,22 +8,17 @@ public class Toycar : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0.0f, 0.0f, 10.0f), ForceMode.Force);
     }
 
     // 物理演算（AddForce）は FixedUpdate にお引越し
     void FixedUpdate()
     {
-        Vector3 forwardDirection = this.transform.forward;
-        rb.AddForce(forwardDirection * forceMagnitude);
+       
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // 地面なら何もしない（早期リターン）
-        if (other.gameObject.CompareTag("ground")) return;
-
-        // Y軸（上向きの軸）を中心に90度回転させる
-        // 面倒な Vector3 への代入をスキップして1行で書けます
-        this.transform.Rotate(0f, 90f, 0f);
+       
     }
 }

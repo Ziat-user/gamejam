18f4c308181e6197b7619740133f7bcc3e53fe92
Yuki-Ikeda-0703 <partenon4126@gmail.com>
2026-06-27 16:39:25 +0900

    あ

--- Assets/C#/MoveCamera.cs
+++ Assets/C#/MoveCamera.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    //public PlayerMove player;
-    //public Toycar toyCar;
+    public PlayerMove player;
+    public Toycar toyCar;
 
-    //public float cameraHeight = 10.0f;
-    //public float cameraBack = -2.0f;
+    public float cameraPlayerHeight = 10.0f;
+    public float cameraPlayerBack = -2.0f;
+    
+    public float cameraToyHeight = 10.0f;
+    public float cameraToyBack = -2.0f;
 
+    public TimerManager tm;
 
-    //void Start()
-    //{
+    void Start()
+    {
 
-    //}
-    //void Update()
-    //{
-    //    if (player.GetFlag)
-    //    {
-    //        Vector3 cameraPlayerPos = player.transform.position;
-    //        cameraPlayerPos.y += cameraHeight;
-    //        cameraPlayerPos.z += cameraBack;
-    //    }
+    }
+    void Update()
+    {
+        if (tm.isPlayer)
+        {
+            Vector3 cameraPlayerPos = player.transform.position;
+            transform.LookAt(cameraPlayerPos);
+            cameraPlayerPos.y += cameraPlayerHeight;
+            cameraPlayerPos.z += cameraPlayerBack;
+            transform.position = cameraPlayerPos;
+            //Vector3 cameraAngle = new Vector3(90.0f, 0.0f, 0.0f);
+            //transform.eulerAngles = cameraAngle;
+        }
 
-    //    if (toyCar.GetFlag)
-    //    {
-    //        Vector3 cameraToyPos = toyCar.transform.position;
-
-    //    }
-    //}
+        if (tm.isToyCar)
+        {
+            Vector3 cameraToyPos = toyCar.transform.position;
+            transform.LookAt(cameraToyPos);
+            cameraToyPos.y += cameraToyHeight;
+            cameraToyPos.z += cameraToyBack;
+            transform.position = cameraToyPos;
+        }
+    }
 }
